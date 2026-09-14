f15829e91f8526b54120cd3bf6e5c5d1791ce9df
mediaflow0548 <mediaflow0548@outlook.com>
2026-09-14 21:35:53 +0200

    Link juridische footerpagina's naar actieve taal

--- script.js
+++ script.js
@@ -369,8 +369,8 @@ function buildFooter() {
             </div>
             <div class="footer-legal">
                 <h4>${t.legalTitle}</h4>
-                <a href="${rootPrefix}privacyverklaring.html">${t.privacy}</a>
-                <a href="${rootPrefix}disclaimer.html">${t.disclaimer}</a>
+                <a href="${rootPrefix}${currentLanguage()}/privacyverklaring.html">${t.privacy}</a>
+                <a href="${rootPrefix}${currentLanguage()}/disclaimer.html">${t.disclaimer}</a>
             </div>
         </div>
         <div class="footer-certificate">
